6e393824007a9179d958d5fa2a16c5db5172c297
15837905428 <68308558+15837905428@users.noreply.github.com>
2022-09-07 18:49:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  // APP_ID: "wx7ca23e3961d06c29",
+   APP_ID: "wx7ca23e3961d06c29",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  // APP_SECRET: "7a05abe3271c7df2752df10197150dfa",
+   APP_SECRET: "7a05abe3271c7df2752df10197150dfa",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
